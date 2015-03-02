498ae5af42e4031b6a52717bc2ec446ebee05efb
Harshit Agarwal <harshitagarwal90@gmail.com>
2015-03-01 16:59:12 -0800

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -168,7 +168,7 @@ static const int GRID_COLUMNS = 10;
             else if (currentCreature.livingNeighbors <= 1 || currentCreature.livingNeighbors >= 4) {
                 currentCreature.isAlive = false;
             }
-            
+
             if (currentCreature.isAlive) {
                 numAlive += 1;
             }
